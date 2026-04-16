871cbdb5d15c4af94b5b1179eebb8064046da826
PancakeMarquis <diego18902@gmail.com>
2026-04-15 23:06:02 -0600

    Add HTMLNode and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,19 +5,30 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertEqual(node, node2)
-        node3 = TextNode("This is a text node", TextType.ITALIC)
-        node4 = TextNode("This is a text node", TextType.ITALIC)
-        self.assertEqual(node3, node4)
-        node5 = TextNode("This is a text node", TextType.CODE)
-        node6 = TextNode("This is a text node", TextType.CODE)
-        self.assertEqual(node5, node6)
-    def test_neq(self):
-        node7 = TextNode("This is a text node", TextType.LINK, "boot.dev")
-        node8 = TextNode("This is a text node", TextType.IMAGE, "boot.dev")
-        self.assertNotEqual(node7, node8)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 
 
